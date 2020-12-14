3fd1d980e79be8a8bdb62b98ae242d4fe3fcf68e
whimmy <whimmy@126.com>
2020-12-14 14:50:28 +0800

    add cartesian

--- spark-operations/src/main/scala/com/ymmihw/transformation/CartesianTransformation.scala
+++ spark-operations/src/main/scala/com/ymmihw/transformation/CartesianTransformation.scala
@@ -3,14 +3,14 @@ package com.ymmihw.transformation
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.{SparkConf, SparkContext}
 
-object UnionTransformation {
+object CartesianTransformation {
   def main(args: Array[String]) {
-    val conf = new SparkConf().setAppName("UnionTransformation").setMaster("local[*]")
+    val conf = new SparkConf().setAppName("CartesianTransformaion").setMaster("local[*]")
     val spark = SparkSession.builder.appName("Simple Application").config(conf).getOrCreate()
     val sc = spark.sparkContext
-    val a = sc.parallelize(List("V1", "V2", "U1", "U2", "U3", "U4"), 2)
-    val b = sc.parallelize(List("V1", "V8", "U5", "U6", "U7", "U8"), 2)
-    var c = (a ++ b).collect
+    val a = sc.parallelize(List("V1", "V2", "U1", "U2"), 2)
+    val b = sc.parallelize(List("W1", "W2", "Q5"), 2)
+    var c = a.cartesian(b).collect
     println(c.mkString(", "))
     spark.stop()
   }
