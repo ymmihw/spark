06e5066174520ba01f7b695553d72edf5525f14c
whimmy <whimmy@126.com>
2020-12-14 17:40:05 +0800

    add distinct

---
+++ spark-operations/src/main/scala/com/ymmihw/transformation/DistinctTransformation.scala
@@ -0,0 +1,16 @@
+package com.ymmihw.transformation
+
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+
+object DistinctTransformation {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("DistinctTransformation").setMaster("local[*]")
+    val spark = SparkSession.builder.appName("Simple Application").config(conf).getOrCreate()
+    val sc = spark.sparkContext
+    val a = sc.parallelize(List("V1", "V1", "V3", "U1", "U1", "U1", "U2", "U2"), 2)
+    var b = a.distinct.collect
+    println(b.mkString(", "))
+    spark.stop()
+  }
+}
