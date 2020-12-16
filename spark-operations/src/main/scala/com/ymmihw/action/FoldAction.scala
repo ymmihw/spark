2cda0dfda8c0fd98968be9c3d24091873f2006ab
whimmy <whimmy@126.com>
2020-12-16 13:29:30 +0800

    add FoldAction

--- spark-operations/src/main/scala/com/ymmihw/action/FoldAction.scala
+++ spark-operations/src/main/scala/com/ymmihw/action/FoldAction.scala
@@ -3,13 +3,13 @@ package com.ymmihw.action
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 
-object ReduceAction {
+object FoldAction {
   def main(args: Array[String]) {
-    val conf = new SparkConf().setAppName("ReduceAction").setMaster("local[*]")
+    val conf = new SparkConf().setAppName("FoldAction").setMaster("local[*]")
     val spark = SparkSession.builder.appName("Simple Application").config(conf).getOrCreate()
     val sc = spark.sparkContext
     val x = sc.parallelize(List(("V1", 1), ("V3", 2), ("U1", 2), ("U2", 2), ("U3", 4), ("U4", 1)), 2)
-    val y = x.reduce((a, b) => (a._1 + "@" + b._1, a._2 + b._2))
+    val y = x.fold(("V0", 0))((a, b) => (a._1 + "@" + b._1, a._2 + b._2))
     println(y)
     spark.stop()
   }
