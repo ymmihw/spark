88bc7c09de41eaa26f1593f09c85b9fd96a1e4ed
whimmy <whimmy@126.com>
2020-12-16 11:58:38 +0800

    add CountAction

---
+++ spark-operations/src/main/scala/com/ymmihw/action/CountAction.scala
@@ -0,0 +1,16 @@
+package com.ymmihw.action
+
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+
+object CountAction {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("ForeachAction").setMaster("local[*]")
+    val spark = SparkSession.builder.appName("Simple Application").config(conf).getOrCreate()
+    val sc = spark.sparkContext
+    val a = sc.parallelize(List("V1", "V2", "V3", "U1", "U2"), 2)
+    val b = a.count;
+    println(b)
+    spark.stop()
+  }
+}
