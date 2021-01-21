7b88e93c4b62bd2243fab84d4a9548f7c89487cf
wanghaoming <wanghaoming@ctfo.com>
2021-01-21 10:48:33 +0800

    read from classpath

--- spark-helloworld/src/main/scala/com/ymmihw/CsvApp.scala
+++ spark-helloworld/src/main/scala/com/ymmihw/CsvApp.scala
@@ -1,14 +1,18 @@
-package com.ymmihw;
+package com.ymmihw
+
+;
+
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType};
+
 object CsvApp {
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("CsvApp").setMaster("local[*]")
     val sqlContext = SparkSession.builder().config(conf).getOrCreate()
     val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
     val customizedSchema = StructType(Array(StructField("gender", StringType, true), StructField("race", StringType, true), StructField("parentalLevelOfEducation", StringType, true), StructField("lunch", StringType, true), StructField("testPreparationCourse", StringType, true), StructField("mathScore", IntegerType, true), StructField("readingScore", IntegerType, true), StructField("writingScore", IntegerType, true)))
-    val pathToFile = "spark-helloworld/StudentsPerformance.csv"
+    val pathToFile = CsvApp.getClass.getClassLoader.getResource("StudentsPerformance.csv").toString
     val DF = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").schema(customizedSchema).load(pathToFile)
     print("We are starting from here...!")
     DF.rdd.cache()
--- spark-helloworld/src/main/scala/com/ymmihw/ReadMeApp.scala
+++ spark-helloworld/src/main/scala/com/ymmihw/ReadMeApp.scala
@@ -5,7 +5,8 @@ import org.apache.spark.sql.SparkSession;
 
 object ReadMeApp {
   def main(args: Array[String]) {
-    val logFile = "spark-helloworld/README.md" // Should be some file on your system
+//    val logFile = "spark-helloworld/README.md" // Should be some file on your system
+    val logFile = CsvApp.getClass.getClassLoader.getResource("README.md").toString
     val conf = new SparkConf().setAppName("ReadMeApp").setMaster("local[*]")
     val spark = SparkSession.builder.appName("Simple Application").config(conf).getOrCreate()
     val logData = spark.read.textFile(logFile).cache()
