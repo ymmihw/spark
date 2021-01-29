9e68a5277849e984366e175a4cced6e2fe0a4595
wanghaoming <wanghaoming@ctfo.com>
2021-01-29 09:17:18 +0800

    clean up

--- spark-sql/by-examples-pivot-unpivot/src/main/scala/com/ymmihw/PivotExample.scala
+++ spark-sql/by-examples-pivot-unpivot/src/main/scala/com/ymmihw/PivotExample.scala
@@ -2,29 +2,28 @@ package com.ymmihw
 
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
+
 object PivotExample {
-  def main(args:Array[String]):Unit= {
+  def main(args: Array[String]): Unit = {
 
     val spark: SparkSession = SparkSession.builder()
       .master("local[1]")
       .appName("SparkByExamples.com")
       .getOrCreate()
 
-    val data = Seq(("Banana",1000,"USA"), ("Carrots",1500,"USA"), ("Beans",1600,"USA"),
-      ("Orange",2000,"USA"),("Orange",2000,"USA"),("Banana",400,"China"),
-      ("Carrots",1200,"China"),("Beans",1500,"China"),("Orange",4000,"China"),
-      ("Banana",2000,"Canada"),("Carrots",2000,"Canada"),("Beans",2000,"Mexico"))
+    val data = Seq(("Banana", 1000, "USA"), ("Carrots", 1500, "USA"), ("Beans", 1600, "USA"),
+      ("Orange", 2000, "USA"), ("Orange", 2000, "USA"), ("Banana", 400, "China"),
+      ("Carrots", 1200, "China"), ("Beans", 1500, "China"), ("Orange", 4000, "China"),
+      ("Banana", 2000, "Canada"), ("Carrots", 2000, "Canada"), ("Beans", 2000, "Mexico"))
 
     import spark.sqlContext.implicits._
-    val df = data.toDF("Product","Amount","Country")
+    val df = data.toDF("Product", "Amount", "Country")
     df.show()
 
     //pivot
-    val pivotDF = df.groupBy("Product","Country")
-      .sum("Amount")
-      .groupBy("Product")
+    val pivotDF = df.groupBy("Product")
       .pivot("Country")
-      .sum("sum(Amount)")
+      .sum("Amount")
     pivotDF.show()
 
     //    val countries = Seq("USA","China","Canada","Mexico")
